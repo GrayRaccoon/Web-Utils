83a4836ae9975fc89d3e2ef01c5142aa0a82db40
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-04-25 12:22:42 -0500

    Added Default Constructor to GenericDTO.

--- src/main/java/com/grayraccoon/webutils/dto/GenericDto.java
+++ src/main/java/com/grayraccoon/webutils/dto/GenericDto.java
@@ -2,15 +2,13 @@ package com.grayraccoon.webutils.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.grayraccoon.webutils.errors.ApiError;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Builder
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 @JsonInclude(JsonInclude.Include.NON_NULL)
